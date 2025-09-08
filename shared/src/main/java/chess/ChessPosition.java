18c6e705311132d42281b6f702c9d0774d13bf1b
Adelyn Meeks <honey17@byu.edu>
2025-09-08 12:16:57 -0600

    added override to position

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -39,4 +39,15 @@ public class ChessPosition {
         return col;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
 }
