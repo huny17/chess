14524a371833c0305a5b4315bff9f444633d78c8
Adelyn Meeks <honey17@byu.edu>
2025-10-10 13:14:31 -0600

    working on gamedata class

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,4 +1,68 @@
 package model;
 
+import chess.ChessGame;
+
+import java.util.Objects;
+
 public class GameData {
+
+    private final int id;
+    private final String whiteUser;
+    private final String blackUser;
+    private final String name;
+    private final ChessGame game;
+
+    GameData(int ID, String whiteUser, String blackUser, String name, ChessGame game){
+        id = ID;
+        this.whiteUser = whiteUser;
+        this.blackUser = blackUser;
+        this.name = name;
+        this.game = game;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getWhiteUser(){
+        return whiteUser;
+    }
+
+    public String getBlackUser(){
+        return blackUser;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public ChessGame getGame(){
+        return game;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GameData gameData = (GameData) o;
+        return id == gameData.id && Objects.equals(whiteUser, gameData.whiteUser) && Objects.equals(blackUser, gameData.blackUser) && Objects.equals(name, gameData.name) && Objects.equals(game, gameData.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, whiteUser, blackUser, name, game);
+    }
+
+    @Override
+    public String toString() {
+
+        String str = String.format("W");
+
+        return "GameData{" +
+                "id=" + id +
+                ", whiteUser='" + whiteUser + '\'' +
+                ", blackUser='" + blackUser + '\'' +
+                ", name='" + name + '\'' +
+                ", game=" + game +
+                '}';
+    }
 }
