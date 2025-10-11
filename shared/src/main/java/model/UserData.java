5995a06e602ea17fde40aec302c9ed9bd70653c6
Adelyn Meeks <honey17@byu.edu>
2025-10-11 15:28:45 -0600

    working on userdata class

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,4 +1,26 @@
 package model;
 
 public class UserData {
+
+    private final String name;
+    private final String password;
+    private final String email;
+
+    UserData(String name, String password, String email){
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
