801eeb068c19593262a0a44a4406ab4b342f2e9f
Adelyn Meeks <honey17@byu.edu>
2025-10-11 15:31:30 -0600

    working on userdata class string

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class UserData {
 
     private final String name;
@@ -22,5 +24,22 @@ public class UserData {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserData userData = (UserData) o;
+        return Objects.equals(name, userData.name) && Objects.equals(password, userData.password) && Objects.equals(email, userData.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password, email);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s, Password: %s, Email: %s,", name, password, email);
     }
 }
