6908d5bf71bd36df1e391558d6c185f2cbd53426
Adelyn Meeks <honey17@byu.edu>
2025-10-16 13:39:14 -0600

    initializing clear service

--- server/src/main/java/services/ClearService.java
+++ server/src/main/java/services/ClearService.java
@@ -1,4 +1,26 @@
 package services;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import model.AuthData;
+
 public class ClearService {
+
+    private UserDAO userDataAccess;
+    private AuthDAO authDataAccess;
+    private final GameDAO gameAuthData;
+
+    public ClearService(UserDAO userDataAccess, AuthDAO authDataAccess, GameDAO gameAuthData){
+        this.userDataAccess = userDataAccess;
+        this.authDataAccess = authDataAccess;
+        this.gameAuthData = gameAuthData;
+    }
+
+    public void clear(){
+        userDataAccess.clear();
+        authDataAccess.clear();
+        gameAuthData.clear();
+    }
+
 }
