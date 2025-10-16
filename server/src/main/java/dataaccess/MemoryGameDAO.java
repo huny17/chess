2cff170bc4c8e7aaaa1a6260e0652087d45ed9f7
Adelyn Meeks <honey17@byu.edu>
2025-10-16 16:29:13 -0600

    working on DAOs, confused on expected formatting for GameDAO

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.GameData;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class MemoryGameDAO implements GameDAO{
 
@@ -11,5 +12,25 @@ public class MemoryGameDAO implements GameDAO{
     @Override
     public void clear(){
         games.clear();
+    }
+
+    @Override
+    public void createGame(GameData game) {
+        games.put();
+    }
+
+    @Override
+    public GameData getGame(GameData game) {
+        return games.get(game);
+    }
+
+    @Override
+    public List listGames() {
+        return List.of(games);
+    }
+
+    @Override
+    public void updateGame() {
+
     }
 }
