951aa49fefdc0b99244c4580d637c53799cf17a6
Adelyn Meeks <honey17@byu.edu>
2025-10-27 12:58:50 -0600

    Talked to TA. Trying to make a class that contains functions that will be repeatedly called

---
+++ server/src/main/java/dataaccess/ExecuteDBUpdates.java
@@ -0,0 +1,47 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.sql.*;
+
+import static java.sql.Types.NULL;
+
+public class ExecuteDBUpdates {
+
+    public int executeSQLUpdate(String statement, Object... params) throws DataAccessException{ //obj .. param ~ array
+        try(Connection con = DatabaseManager.getConnection()){
+            try(PreparedStatement ps = con.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)){ // sending to databse using sql to update
+                for(int i = 0; i < params.length; i++){  //reading through to update
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p); // Not sure what this section does
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p); //userdata does not have integer, is this a difference in pet class or a sql thing?
+                    else if (param instanceof  UserData p) ps.setString(i + 1, p.toString());
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()){
+                    return rs.getInt(1);
+                }
+                return 0;
+            }
+        } catch (SQLException e){
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    public void configureDatabase(String[] createStatements) throws DataAccessException{
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()){
+            for (String statement : createStatements){
+                try (var preparedStatement = conn.prepareStatement(statement)){
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch(SQLException e){
+            throw new DataAccessException(String.format("Unable to configure database: %s", e.getMessage()));
+        }
+    }
+
+}
--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -3,10 +3,10 @@ package dataaccess;
 import model.UserData;
 import com.google.gson.Gson;
 import model.UserData;
-
+import ExecuteDBUpdates.java;
 import java.sql.*;
 import java.util.HashMap;
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 import static java.sql.Types.NULL;
 
 public class MySQLUserDAO implements UserDAO {
@@ -20,7 +20,7 @@ public class MySQLUserDAO implements UserDAO {
 
     @Override
     public UserData createUser(UserData user) {
-        var statement = "INSERT INTO user (name, password, email) VALUES (?, ?, ?)";
+        var statement = "INSERT INTO user (name, password, email, json) VALUES (?, ?, ?)"; //How is json being used?
         String json = new Gson().toJson(user);
         return new UserData(user.username(), user.password(), user.email());
     }
@@ -33,7 +33,7 @@ public class MySQLUserDAO implements UserDAO {
                 ps.setString(1, username); //getting value
                 try (ResultSet rs = ps.executeQuery()) {
                     if (rs.next()) {
-                        return readPet(rs);
+                        return readUser(rs);
                     }
                 }
             }
@@ -45,9 +45,26 @@ public class MySQLUserDAO implements UserDAO {
 
     private UserData readUser(ResultSet rs) throws SQLException{
         var username = rs.getString("username");
-        var
-        UserData user = new Gson().fromJson(json, UserData.class);
+        var password = rs.getString("password");
+        var email = rs.getString("email");
+        return new UserData(username, password, email);
     }
+
+    public void deleteUser(String username) throws ResponseException{
+        var statement = "DELETE FROM user WHERE username=?";
+        executeUpdate(statement, username);
+    }
+
+    public void deleteAllUsers() throws Exception{
+        String statement = "TRUNCATE user";
+        executeUpdate(statement);
+    }
+
+    //make into class to call method from
+
+
+    /// recommended creating tables here?
+
 
     @Override
     public HashMap<String, UserData> getUserMap(){
