f50160ecdb16dd244e87a88f4a36114500985e89
Adelyn Meeks <honey17@byu.edu>
2025-10-31 14:15:36 -0600

    wrote data access tests

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -1,6 +1,6 @@
 package dataaccess;
 
-import chess.ChessGame;
+import chess.*;
 import model.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import passoff.server.TestServerFacade;
 import server.Server;
 
+import static org.eclipse.jetty.util.LazyList.isEmpty;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataAccessTest {
@@ -17,22 +18,28 @@ class DataAccessTest {
     private static final GameData joinedBlackGame = new GameData(1, null, "joe", "test", new ChessGame());
     private static Server server;
     private static TestServerFacade fakeServer;
-    private static UserDAO uAccess;
-    private static AuthDAO aAccess;
-    private static GameDAO gAccess;
+    private static UserDAO userAccess;
+    private static AuthDAO authAccess;
+    private static GameDAO gameAccess;
 
 @BeforeAll
 public static void configureDatabase() throws DataAccessException{
     server = new Server();
     var port = server.run(0);
     fakeServer = new TestServerFacade("localhost", Integer.toString(port));
-    uAccess = new MySQLUserDAO();
-    aAccess = new MySQLAuthDAO();
-    gAccess = new MySQLGameDAO();
+    userAccess = new MySQLUserDAO();
+    authAccess = new MySQLAuthDAO();
+    gameAccess = new MySQLGameDAO();
 }
 
 @BeforeEach
-public void preClear(){fakeServer.clear();}
+public void preClear() throws DataAccessException{
+    fakeServer.clear();
+    userAccess.clear();
+    authAccess.clear();
+    gameAccess.clear();
+}
+
 
 @AfterAll
 static void stop(){server.stop();}
@@ -40,81 +47,169 @@ static void stop(){server.stop();}
 @Test
  void clear() {
     assertDoesNotThrow(()-> {
-         uAccess.createUser(user);
-         assertNotNull(uAccess.getUser(user.username()));
-         uAccess.clear();
-         assertNull(uAccess.getUser(user.username()));
+         userAccess.createUser(user);
+         assertNotNull(userAccess.getUser(user.username()));
+         assertNotNull(authAccess);
+         authAccess.createAuth(new AuthData("c", "123"));
+         gameAccess.createGame("test");
+         assertNotNull(gameAccess.getGame(1));
+         userAccess.clear();
+         gameAccess.clear();
+         authAccess.clear();
+         assertNull(userAccess.getUser(user.username()));
+         assertNull(authAccess.getUser("123"));
+         assertNull(gameAccess.getGame(1));
      });
  }
 
  @Test
  void createUser() {
      assertDoesNotThrow(()-> {
-         uAccess.createUser(user);
-         assertNotNull(uAccess);
+         userAccess.createUser(user);
+         assertNotNull(userAccess);
      });
  }
 
     @Test
-    void injectionDefense() {
+    void userInjectionDefense() {
     assertDoesNotThrow(()-> {
         UserData injection = new UserData("joe","123","c@c); DROP TABLE user; -- ");
-        uAccess.createUser(injection);
-        assertNotNull(uAccess);
+        userAccess.createUser(injection);
+        assertNotNull(userAccess);
     });
     }
 
-    //check
+    @Test
+    void createAuth() {
+        assertDoesNotThrow(()-> {
+            userAccess.createUser(user);
+            assertNotNull(authAccess);
+        });
+    }
+
+    @Test
+    void longAuth(){
+            AuthData auth = new AuthData("joe","some really long thing, not sure how long I officially need to make it but like it needs to be unreasonably long almost like it needs to cause a length issue as was set when the table was made.");
+            assertThrows(DataAccessException.class, ()-> authAccess.createAuth(auth));
+    }
+
+    @Test
+    void deleteAuth() {
+        assertDoesNotThrow(()-> {
+            AuthData auth = new AuthData("joe","token");
+            authAccess.createAuth(auth);
+            assertNotNull(authAccess);
+            authAccess.deleteAuth("token");
+            assertNotSame(authAccess.getAuth("token"), auth);
+        });
+    }
+
+    @Test
+    void authInjectionDefense() {
+        assertDoesNotThrow(()-> {
+            AuthData injection = new AuthData("joe","123); DROP TABLE user; -- ");
+            authAccess.createAuth(injection);
+            assertNotNull(authAccess);
+        });
+    }
 
  @Test
  void createGame() {
      assertDoesNotThrow(()-> {
-        uAccess.createUser(user);
-        gAccess.createGame("test");
-        assertNotNull(gAccess);
+        userAccess.createUser(user);
+        gameAccess.createGame("test");
+        assertNotNull(gameAccess);
      });
  }
 
     @Test
+    void longGameName(){
+        assertDoesNotThrow(()->userAccess.createUser(user));
+        assertThrows(DataAccessException.class, ()-> gameAccess.createGame("some really long thing, not sure how long I officially need to make it but like it needs to be unreasonably long almost like it needs to cause a length issue as was set when the table was made."));
+    }
+
+    @Test
     void getGame() {
         assertDoesNotThrow(()-> {
-            uAccess.createUser(user);
-            gAccess.createGame("test");
-            assertNotNull(gAccess.getGame(1));
+            userAccess.createUser(user);
+            gameAccess.createGame("test");
+            assertNotNull(gameAccess.getGame(1));
         });
     }
 
     @Test
     void getWhiteUsername() {
         assertDoesNotThrow(()-> {
-            uAccess.createUser(user);
-            gAccess.createGame("test");
-            gAccess.updateWhiteTeam("1", joinedWhiteGame);
-            assertNotNull(gAccess.getWhiteUser("1"));
+            userAccess.createUser(user);
+            gameAccess.createGame("test");
+            gameAccess.updateWhiteTeam("1", joinedWhiteGame);
+            assertNotNull(gameAccess.getWhiteUser("1"));
+            gameAccess.createGame("number2");
+            assertNull(gameAccess.getWhiteUser("2"));
         });
     }
 
     @Test
     void getBlackUsername() {
         assertDoesNotThrow(()-> {
-            uAccess.createUser(user);
-            gAccess.createGame("test");
-            gAccess.updateBlackTeam("1", joinedBlackGame);
-            assertNotNull(gAccess.getBlackUser("1"));
-            assertEquals("joe", gAccess.getBlackUser("1"));
+            userAccess.createUser(user);
+            gameAccess.createGame("test");
+            gameAccess.updateBlackTeam("1", joinedBlackGame);
+            assertNotNull(gameAccess.getBlackUser("1"));
+            assertEquals("joe", gameAccess.getBlackUser("1"));
+            gameAccess.createGame("number2");
+            assertNull(gameAccess.getBlackUser("2"));
+        });
+    }
+
+    @Test
+    void updateGame() {
+        assertDoesNotThrow(()-> {
+            userAccess.createUser(user);
+            gameAccess.createGame("test");
+            assertNotNull(gameAccess.getGame(1));
+            ChessBoard board = new ChessGame().getBoard();
+            ChessGame update = new ChessGame();
+            update.setBoard(board);
+            update.makeMove(new ChessMove(new ChessPosition(2,3), new ChessPosition(4,3),null));
+            GameData updateGame = new GameData(1, null, "joe", "test", update);
+            gameAccess.updateGame("1", updateGame);
+            GameData theGame = gameAccess.getGame(1);
+            assertEquals(theGame.chessGame(), update);
+        });
+    }
+
+    @Test
+    void joinGame() {
+        assertDoesNotThrow(()-> {
+            userAccess.createUser(user);
+            gameAccess.createGame("test");
+            assertNotNull(gameAccess.getGame(1));
+            GameData updateGame = new GameData(1, null, "joe", "test", new ChessGame());
+            gameAccess.updateBlackTeam("1", updateGame);
+            GameData theGame = gameAccess.getGame(1);
+            assertEquals(theGame.blackUsername(), user.username());
         });
     }
 
     @Test
     void listGames() {
         assertDoesNotThrow(()-> {
-            uAccess.createUser(user);
-            gAccess.createGame("test");
-            gAccess.createGame("Aye");
-            gAccess.createGame("yo");
-            gAccess.createGame("123");
-            assertNotNull(gAccess.listGames());
-            assertEquals(4, gAccess.listGames().toArray().length);
+            userAccess.createUser(user);
+            gameAccess.createGame("test");
+            gameAccess.createGame("Aye");
+            gameAccess.createGame("yo");
+            gameAccess.createGame("123");
+            assertNotNull(gameAccess.listGames());
+            assertEquals(4, gameAccess.listGames().toArray().length);
+        });
+    }
+
+    @Test
+    void listEmptyGames() {
+        assertDoesNotThrow(()-> {
+            userAccess.createUser(user);
+            assertEquals(0, gameAccess.listGames().toArray().length);
         });
     }
 
