7b23f543be0599160cf33b4e21e024f56a1b1fef
Adelyn Meeks <honey17@byu.edu>
2025-10-31 13:03:37 -0600

    figured out negative tests to write

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -17,12 +17,18 @@ class DataAccessTest {
     private static final GameData joinedBlackGame = new GameData(1, null, "joe", "test", new ChessGame());
     private static Server server;
     private static TestServerFacade fakeServer;
+    private static UserDAO uAccess;
+    private static AuthDAO aAccess;
+    private static GameDAO gAccess;
 
 @BeforeAll
-public static void configureDatabase(){
+public static void configureDatabase() throws DataAccessException{
     server = new Server();
     var port = server.run(0);
     fakeServer = new TestServerFacade("localhost", Integer.toString(port));
+    uAccess = new MySQLUserDAO();
+    aAccess = new MySQLAuthDAO();
+    gAccess = new MySQLGameDAO();
 }
 
 @BeforeEach
@@ -34,91 +40,81 @@ static void stop(){server.stop();}
 @Test
  void clear() {
     assertDoesNotThrow(()-> {
-     UserDAO ua = new MySQLUserDAO();
-         ua.createUser(user);
-         assertNotNull(ua.getUser(user.username()));
-         ua.clear();
-         assertNull(ua.getUser(user.username()));
+         uAccess.createUser(user);
+         assertNotNull(uAccess.getUser(user.username()));
+         uAccess.clear();
+         assertNull(uAccess.getUser(user.username()));
      });
  }
 
  @Test
  void createUser() {
      assertDoesNotThrow(()-> {
-     UserDAO ua = new MySQLUserDAO();
-         ua.createUser(user);
-         assertNotNull(ua);
+         uAccess.createUser(user);
+         assertNotNull(uAccess);
      });
  }
 
-//    @Test
-//    void badCreateUser() {
-//        //UserDAO da;
-//        try {
-//            UserDAO da = new MySQLUserDAO();
-//            assertThrows(da.createUser(null));
-//        }
-//    }
+    @Test
+    void injectionDefense() {
+    assertDoesNotThrow(()-> {
+        UserData injection = new UserData("joe","123","c@c); DROP TABLE user; -- ");
+        uAccess.createUser(injection);
+        assertNotNull(uAccess);
+    });
+    }
+
+    //check
 
  @Test
  void createGame() {
      assertDoesNotThrow(()-> {
-        UserDAO ua = new MySQLUserDAO();
-        GameDAO ga = new MySQLGameDAO();
-        ua.createUser(user);
-        ga.createGame("test");
-        assertNotNull(ga);
+        uAccess.createUser(user);
+        gAccess.createGame("test");
+        assertNotNull(gAccess);
      });
  }
 
     @Test
     void getGame() {
         assertDoesNotThrow(()-> {
-            UserDAO ua = new MySQLUserDAO();
-            GameDAO ga = new MySQLGameDAO();
-            ua.createUser(user);
-            ga.createGame("test");
-            assertNotNull(ga.getGame(1));
+            uAccess.createUser(user);
+            gAccess.createGame("test");
+            assertNotNull(gAccess.getGame(1));
         });
     }
 
     @Test
     void getWhiteUsername() {
         assertDoesNotThrow(()-> {
-            UserDAO ua = new MySQLUserDAO();
-            GameDAO ga = new MySQLGameDAO();
-            ua.createUser(user);
-            ga.createGame("test");
-            ga.updateWhiteTeam("1", joinedWhiteGame);
-            assertNotNull(ga.getWhiteUser("1"));
+            uAccess.createUser(user);
+            gAccess.createGame("test");
+            gAccess.updateWhiteTeam("1", joinedWhiteGame);
+            assertNotNull(gAccess.getWhiteUser("1"));
         });
     }
 
     @Test
     void getBlackUsername() {
         assertDoesNotThrow(()-> {
-            UserDAO ua = new MySQLUserDAO();
-            GameDAO ga = new MySQLGameDAO();
-            ua.createUser(user);
-            ga.createGame("test");
-            ga.updateBlackTeam("1", joinedBlackGame);
-            assertNotNull(ga.getBlackUser("1"));
-            assertEquals("joe", ga.getBlackUser("1"));
+            uAccess.createUser(user);
+            gAccess.createGame("test");
+            gAccess.updateBlackTeam("1", joinedBlackGame);
+            assertNotNull(gAccess.getBlackUser("1"));
+            assertEquals("joe", gAccess.getBlackUser("1"));
         });
     }
 
     @Test
     void listGames() {
         assertDoesNotThrow(()-> {
-            UserDAO ua = new MySQLUserDAO();
-            GameDAO ga = new MySQLGameDAO();
-            ua.createUser(user);
-            ga.createGame("test");
-            ga.createGame("Aye");
-            ga.createGame("yo");
-            ga.createGame("123");
-            assertNotNull(ga.listGames());
-            assertEquals(4, ga.listGames().toArray().length);
+            uAccess.createUser(user);
+            gAccess.createGame("test");
+            gAccess.createGame("Aye");
+            gAccess.createGame("yo");
+            gAccess.createGame("123");
+            assertNotNull(gAccess.listGames());
+            assertEquals(4, gAccess.listGames().toArray().length);
         });
     }
 
