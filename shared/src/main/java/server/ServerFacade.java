94507a60c23cd0dc2b8820bc5afc230fb458974a
Adelyn Meeks <honey17@byu.edu>
2025-11-05 13:33:37 -0700

    working on http handlers

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,6 +1,17 @@
 package server;
 
+import com.google.gson.Gson;
+import model.*;
+
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.util.Collection;
+
+import java.net.*;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
@@ -10,6 +21,27 @@ public class ServerFacade {
         serverUrl = url;
     }
 
+    public Collection<GameData> listGames() throws Exception{
+        var request = buildRequest("GET", "", ):
+        var response = sendRequest(request);
+        return handleResponse(response, )
+    }
 
+    private HttpRequest buildRequest(String method, String path, Object body){
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if(body != null){
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
 
+    private BodyPublisher makeRequestBody(Object request){
+        if(request != null){
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
 }
