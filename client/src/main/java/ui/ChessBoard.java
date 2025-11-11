cb8e45bd5079d22d5c94a1bff1aad78c22f23d22
Adelyn Meeks <honey17@byu.edu>
2025-11-11 13:48:11 -0700

    started creating chess board class

---
+++ client/src/main/java/ui/ChessBoard.java
@@ -0,0 +1,9 @@
+package ui;
+
+public class ChessBoard {
+
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
+    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 8;
+    private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
+
+}
