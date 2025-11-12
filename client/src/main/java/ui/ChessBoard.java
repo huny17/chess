9079be2d99e6d43a599015effaf97cbc716b34b9
Adelyn Meeks <honey17@byu.edu>
2025-11-11 21:02:43 -0700

    started creating chess board class

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -1,9 +1,33 @@
 package ui;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import static ui.EscapeSequences.*;
+
 public class ChessBoard {
 
     private static final int BOARD_SIZE_IN_SQUARES = 8;
     private static final int SQUARE_SIZE_IN_PADDED_CHARS = 8;
     private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
+
+    private static final String EMPTY = " ";
+    private static final String piece
+
+    private static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
+        out.print(ERASE_SCREEN);
+
+        drawHeaders(out);
+
+        drawChessBoard(out);
+
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void drawHeaders(PrintStream out){
+
+    }
 
 }
