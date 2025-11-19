6efe45d406aefcb4b9dfc7ae6aff0422727392e3
Adelyn Meeks <honey17@byu.edu>
2025-11-19 15:48:47 -0700

    WebSocketFacade, is this what I want to implement?

--- shared/src/main/java/websocket/WebSocketFacade.java
+++ shared/src/main/java/websocket/WebSocketFacade.java
@@ -1,16 +1,19 @@
 package websocket;
 
 import exceptions.GeneralException;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class WebSocketFacade { //extends Endpoint
 
+    Session session;
+    NotificationHandler notificationHandler;
+
     public WebSocketFacade(String url, NotificationHandler notificationHandler) throws GeneralException{
         try{
             url = url.replace("http","ws");
             URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
         }catch(URISyntaxException e){
             throw new GeneralException(GeneralException.ExceptionType.server, e.getMessage());
         }
