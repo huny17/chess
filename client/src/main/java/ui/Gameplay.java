42d6d1e0dc8ae400db1e0571dbd4933312e750c6
Adelyn Meeks <honey17@byu.edu>
2025-11-23 19:31:14 -0700

    added basic outline

--- client/src/main/java/ui/Gameplay.java
+++ client/src/main/java/ui/Gameplay.java
@@ -1,6 +1,7 @@
 package ui;
 
 import exceptions.GeneralException;
+import model.request.LoginRequest;
 import server.ServerFacade;
 import ui.websocket.WebSocketFacade;
 
@@ -12,5 +13,33 @@ public class Gameplay {
     public Gameplay(ServerFacade server, WebSocketFacade ws) throws GeneralException {
         this.server = server;
         this.ws = ws;
+    }
+
+    public String redraw(String... params) throws GeneralException {
+        if(params.length == 1){
+
+        }
+        throw new GeneralException(GeneralException.ExceptionType.invalid, "Expected: <username> <password>");
+    }
+
+    public String makeMove(String... params) throws GeneralException {
+        if(params.length == 2){
+
+        }
+        throw new GeneralException(GeneralException.ExceptionType.invalid, "Expected: <username> <password>");
+    }
+
+    public String resign(String... params) throws GeneralException {
+        if(params.length == 1){
+
+        }
+        throw new GeneralException(GeneralException.ExceptionType.invalid, "Expected: <username> <password>");
+    }
+
+    public String highlight(String... params) throws GeneralException {
+        if(params.length == 1){
+
+        }
+        throw new GeneralException(GeneralException.ExceptionType.invalid, "Expected: <username> <password>");
     }
 }
