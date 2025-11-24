db29430b8452bb4ae95df382af13dd257cc36490
Adelyn Meeks <honey17@byu.edu>
2025-11-23 21:05:24 -0700

    had an idea for redrawing the board

--- client/src/main/java/ui/Gameplay.java
+++ client/src/main/java/ui/Gameplay.java
@@ -1,9 +1,13 @@
 package ui;
 
 import exceptions.GeneralException;
+import model.GameData;
+import model.request.JoinGameRequest;
 import model.request.LoginRequest;
 import server.ServerFacade;
 import ui.websocket.WebSocketFacade;
+
+import static ui.EscapeSequences.SET_TEXT_COLOR_BLUE;
 
 public class Gameplay {
 
@@ -17,7 +21,21 @@ public class Gameplay {
 
     public String redraw(String... params) throws GeneralException {
         if(params.length == 1){
-
+            try{
+                int id = Integer.parseInt(params[1]);
+                if(!listedGames.containsKey(id)){
+                    throw new GeneralException(GeneralException.ExceptionType.invalid, "That game does not exist yet, try another game.");
+                }
+                GameData findGame = listedGames.get(id);
+                if(findGame != null){
+                    server.joinGame(new JoinGameRequest(params[0].toUpperCase(), findGame.gameID().toString()));
+                    String notification = String.format("You are now playing %s", findGame.gameName());
+                    runGame(params[0], findGame);
+                    ws.makeConnection(server.getToken(), findGame.gameID());
+                    return SET_TEXT_COLOR_BLUE+notification;
+                }
+            }catch(NumberFormatException ignored){
+            }
         }
         throw new GeneralException(GeneralException.ExceptionType.invalid, "Expected: <username> <password>");
     }
