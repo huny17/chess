e6ac1780ebc1a72e36ca07ed915e2740441b84c2
Adelyn Meeks <honey17@byu.edu>
2025-12-01 13:15:58 -0900

    correcting main set up

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,17 +5,17 @@ import ui.theboard.BoardView;
 
 public class Main {
     public static void main(String[] args) throws  GeneralException {
-//        try {
-//            ChessClient client = new ChessClient("http://localhost:8080");
-//            client.run();
-//        }catch(GeneralException e){
-//            throw new GeneralException(GeneralException.ExceptionType.invalid, e.getMessage());
-//        }
+        try {
+            ChessClient client = new ChessClient("http://localhost:8080");
+            client.run();
+        }catch(GeneralException e){
+            throw new GeneralException(GeneralException.ExceptionType.invalid, e.getMessage());
+        }
 
         /** testing the board **/
-        ChessGame g = new ChessGame();
-        BoardView.run(g.getBoard(),"white");
-        BoardView.run(g.getBoard(),"black");
+//        ChessGame g = new ChessGame();
+//        BoardView.run(g.getBoard(),"white");
+//        BoardView.run(g.getBoard(),"black");
 
     }
 }
